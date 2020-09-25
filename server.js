2c374a0407511d036aaf0b474d611b617cd6707d
S R <sharlu@Ss-MacBook.broadband>
2020-09-25 17:35:31 +0100

    Level 1 and Level 2 challenge completed

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const lodash = require("lodash");
+//var cors = require("cors");
 const app = express();
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+//app.use(cors());
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -17,6 +19,26 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+
+//renders all quotes
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+//renders one random quote
+app.get("/quotes/random", function (request, response) {
+  let randomQuote = lodash.sample(quotes);
+  response.send(randomQuote);
+});
+
+//searches a quote
+app.get("/quotes/search", function (request, response) {
+  let queryParam = request.query.term;
+  let searchedQuote = quotes.filter((quote) =>
+    quote.quote.toUpperCase().includes(queryParam.toUpperCase())
+  );
+  response.send(searchedQuote);
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +51,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(3060, () => {
+  console.log("Your app is listening on port 3060"); //+ listener.address().port);
 });
